d7ca9e16ba65bc8b999d7644bcd4c8113c6b453b
orieken <oriekenjr@gmail.com>
2012-10-23 13:57:14 -0400

    added examples for homepage list of examples

---
+++ spec/isolation/home_page_spec.rb
@@ -0,0 +1,13 @@
+require 'rubygems'
+require 'spec_helper'
+
+describe "Home Page Validations" do
+
+    before(:each) do
+      visit "/"
+    end
+
+    it { page.should have_selector('ul#examples') }
+    it { page.should have_selector('li') }
+    it { page.should have_content('Hi') }
+end
