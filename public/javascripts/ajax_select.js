e34ca6a7220d5da70ca6cba43a0c689f460d331e
Oscar Rieken <oriekenjr@gmail.com>
2013-04-05 11:10:28 -0400

    refactoring ajax for select box

---
+++ public/javascripts/ajax_select.js
@@ -0,0 +1,23 @@
+/**
+ * Created with JetBrains RubyMine.
+ * User: orieken
+ * Date: 3/28/13
+ * Time: 2:38 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+var $universal_year = $('#universal_year'),
+    $universal_make = $('#universal_make'),
+    $submit_button = $('#submit_ajax');
+
+
+$universal_year.change(function()
+{
+    option = $universal_year.val();
+    if (option != 'select year') {
+        $universal_make.removeAttr('disabled');
+    }
+    else {
+        $universal_make.attr('disabled', 'disabled');
+    }
+}).trigger('change'); // added trigger to calculate initial universal_year
