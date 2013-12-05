eccb7263bd6e2d70aeff2ee98f77dbaf40765406
Oscar Rieken <oriekenjr@gmail.com>
2013-12-05 13:24:07 -0500

    fixed and jazzed up knockoutjs table example but its in divs :O, but it works fancy

--- public/javascripts/knockoutjs/table_knockout.js
+++ public/javascripts/knockoutjs/table_knockout.js
@@ -2,6 +2,19 @@ $(document).ready(function () {
     function AppViewModel() {
         var self = this;
 
+        var fname = ["Olivia","Charles","James","Walter","Peter"];
+        var pickFirstName = function () {
+            return fname[Math.floor(Math.random() * 5)];
+        };
+        var lname = ["Bond","Dawson","Bishop","Sanchez","Francis"];
+        var pickLastName = function () {
+            return lname[Math.floor(Math.random() * 5)];
+        };
+        var positions = ["Spy","Teacher","Villan","Manager","Student"];
+        var pickPosition = function () {
+            return positions[Math.floor(Math.random() * 5)];
+        };
+
         self.people = ko.observableArray([
             {
                 firstName: 'James',
@@ -11,20 +24,24 @@ $(document).ready(function () {
             {
                 firstName: 'Charles',
                 lastName: 'Bond',
-                position: 'Spy'
+                position: 'Manager'
             },
             {
                 firstName: 'Denise',
                 lastName: 'Bond',
-                position: 'Spy'
+                position: 'Teacher'
             }
         ]);
 
         self.addPerson = function () {
-            self.people.push({ name: "New at " + new Date() });
+            self.people.push({
+                firstName: pickFirstName(),
+                lastName: pickLastName(),
+                position:  pickPosition()
+            });
         };
 
-        self.removePerson = function () {
+        self.removePerson = function() {
             self.people.remove(this);
         }
     }
